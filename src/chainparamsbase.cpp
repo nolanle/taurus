4d75a115d2468091fa5ea4196638e072d43d80c5
Nolan Le <hoanglek13@gmail.com>
2018-02-08 19:00:10 +0700

    change port to 9555

--- src/chainparamsbase.cpp
+++ src/chainparamsbase.cpp
@@ -32,7 +32,7 @@ class CBaseMainParams : public CBaseChainParams
 public:
     CBaseMainParams()
     {
-        nRPCPort = 9998;
+        nRPCPort = 9555;
     }
 };
 static CBaseMainParams mainParams;
@@ -45,7 +45,7 @@ class CBaseTestNetParams : public CBaseChainParams
 public:
     CBaseTestNetParams()
     {
-        nRPCPort = 19998;
+        nRPCPort = 19555;
         strDataDir = "testnet3";
     }
 };
